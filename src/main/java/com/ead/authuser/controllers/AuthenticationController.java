d13843501a7ecb94ee89d254c3e32d9087a6f291
Marcos Vassler <marcosvassoler@finchsolucoes.com.br>
2021-11-08 08:34:18 -0300

    Múltiplas visões aplicadas ao mesmo DTO.

--- src/main/java/com/ead/authuser/controllers/AuthenticationController.java
+++ src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -5,6 +5,7 @@ import com.ead.authuser.enums.UserStatus;
 import com.ead.authuser.enums.UserType;
 import com.ead.authuser.models.UserModel;
 import com.ead.authuser.service.UserService;
+import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,7 +24,7 @@ public class AuthenticationController {
     public UserService userService;
 
     @PostMapping("/signup")
-    public ResponseEntity<Object> registerUser(@RequestBody UserDTO userDTO){
+    public ResponseEntity<Object> registerUser(@RequestBody @JsonView(UserDTO.UserView.RegistrationPost.class) UserDTO userDTO){
         if(userService.existsByUserName(userDTO.getUsername())){
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Erro: Username is already taken.");
         }
